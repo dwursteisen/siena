8c53a7c1e035aec16c79742dd938fb230c2b1f55
dwursteisen <none@none>
2011-05-01 14:43:44 +0200

    Add Inheritance support for persistance : fields are now get using Util.getFields and Util.getField which get fields from class then from super class

--- source/src/main/java/siena/ClassInfo.java
+++ source/src/main/java/siena/ClassInfo.java
@@ -18,12 +18,14 @@ package siena;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import java.util.concurrent.ConcurrentHashMap;
 
 import siena.core.lifecycle.LifeCyclePhase;
